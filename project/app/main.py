7f18c36d5ee520aa827a1ef77395e10e7a794270
shayan <shayan677@gmail.com>
2022-12-21 16:17:17 +0000

    added option to enable 'Day of Week' and 'Year'

--- project/app/main.py
+++ project/app/main.py
@@ -14,9 +14,6 @@ class ImageButton(ButtonBehavior, Image):
     # behaves like a button, which is what this widget is
     pass
 
-class DateText(Label):
-    text = 'Hello'
-
 class ClockApp(ScreenManager): 
     # This class is the root of the app/
 
@@ -28,31 +25,39 @@ class ClockApp(ScreenManager):
     day_of_week_on = False
     year_on = False
 
+    # The date that will be shown on the screen. Initially no date is shown.
+    date_label = Label(pos=(0, dp(-60)))
+
     def __init__(self, **kwargs):
         super(ClockApp, self).__init__(**kwargs)
         # Get the current local time every half a second
         Clock.schedule_interval(self.update_time, 0.5)
+        self.ids.clock_screen_id.add_widget(self.date_label)
 
     def update_time(self, *args):
         # This method gets the current time to be displayed on the screen.
         # The seconds may or may not be retrieved, which depends on the value of
         # 'seconds_on' which the user can toggle in the settings.
 
-        if self.seconds_on:
-            self.current_time = str(datetime.now().strftime('%H:%M:%S'))
-        else:
-            self.current_time = str(datetime.now().strftime('%H:%M'))
+        self.current_time = str(datetime.now().strftime('%H:%M' + (':%S' if self.seconds_on else '')))
 
-    def toggle_seconds(self, instance, seconds_switch_on):
-        self.seconds_on = seconds_switch_on
+    def update_screen(self, instance, switch_value, option):
+        # Whenever the options relating to the date are toggled, this function will update.
+        # Essentially, it uses the date options that are enabled to decided what to display in the main screen.
+        # For example, if 'Day of Week' is on, then this will show, otherwise it won't.
 
-    def toggle_day_and_month(self, instance, day_and_month_switch_on):
-        if day_and_month_switch_on:
-            self.date_label = Label(text=datetime.now().strftime('%d %b'), pos=(0, dp(-60)))
-            self.ids.clock_screen_id.add_widget(self.date_label)
-        else:
-            self.ids.clock_screen_id.remove_widget(self.date_label)
-        self.day_and_month_on = day_and_month_switch_on
+        if option == 'seconds':
+            self.seconds_on = switch_value
+        if option == 'day_+_month':
+            self.day_and_month_on = switch_value
+        if option == 'day_of_week':
+            self.day_of_week_on = switch_value
+        if option == 'year':
+            self.year_on = switch_value
+
+        self.date_label.text = datetime.now().strftime(('%a ' if self.day_of_week_on else '')
+                                                        + ('%d %b ' if self.day_and_month_on else '')
+                                                        + ('%Y' if self.year_on else '')).strip()
 
 class Main(App):
     def build(self):
