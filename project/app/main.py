2049d3b1e8234bf178211bf8a589c0cb90aae1f0
shayan <shayan677@gmail.com>
2022-12-26 15:59:46 +0000

    added stopwatch time in 'hh:mm:ss' format

--- project/app/main.py
+++ project/app/main.py
@@ -10,7 +10,8 @@ from datetime import datetime
 
 class ImageButton(ButtonBehavior, Image):
     # Instead of always having a standard button, we can have an image that
-    # behaves like a button, which is what this widget is
+    # behaves like a button, which is what this widget is. This is for the
+    # 'settings' button
     pass
 
 class ClockApp(BoxLayout): 
@@ -27,8 +28,13 @@ class ClockApp(BoxLayout):
     # The date that will be shown on the screen. Initially no date is shown.
     date_label = Label(pos=(0, dp(-60)))
 
-    # The stopwatch time that is displayed in the app
-    stopwatch_time = NumericProperty(0)
+    # This contains the stopwatch_time in seconds. It is used to get the time in the format 'hh:mm:ss'
+    # which will be displayed in the app.
+    stopwatch_time = 0
+    # This is the variable that will be displayed in the app
+    stopwatch_time_displayed = StringProperty('00:00:00.0')
+    # True if the time reaches '99:59:59.9'
+    stopwatch_time_limit_reached = False
 
 
     def __init__(self, **kwargs):
@@ -80,14 +86,35 @@ class ClockApp(BoxLayout):
         # Reset stopwatch to 0
 
         self.stopwatch_time = 0
+        self.stopwatch_time_displayed = '00:00:00.0'
+
+        # When the reset button is pressed, enable the 'start' button if the stopwatch
+        # has reached the limit.
+        if self.stopwatch_time_limit_reached:
+            self.ids.start_button.disabled = False
+            self.stopwatch_time_limit_reached = False
 
     def update_stopwatch(self, *args):
         # This method is called every 0.1 seconds, so add 0.1 to stopwatch_time
         # every time this method is called
 
-        self.stopwatch_time += 0.1
-        # Truncate the time to 1 decimal place.61
-        self.stopwatch_time = float('%.1f'%(self.stopwatch_time))
+        # Stop the stopwatch when the time reaches '99:59:59.9'
+        if self.stopwatch_time < 359999.8:
+            self.stopwatch_time += 0.1
+
+            # Get the number of minutes and hours
+            # Truncate the time to 1 decimal place.
+            minutes = int(self.stopwatch_time) // 60
+            hours = int(minutes) // 60
+            # Ensure that the time displayed is always in the format 'hh:mm:ss' with a leading fractional second
+            self.stopwatch_time_displayed = ('0' if hours < 10 else '') + str(hours) + ':' + ('0' if minutes % 60 < 10 else '') + str(minutes % 60) + ':' + ('0' if self.stopwatch_time % 60 < 10 else '') + str(float('%.1f'%(self.stopwatch_time % 60)))
+        # If the stopwatch reached '99:59:59.9'
+        else:
+            # Stop the clock schedule
+            Clock.unschedule(self.update_stopwatch)
+            self.ids.stop_button.disabled = True
+            self.stopwatch_time_limit_reached = True
+
 
 class Main(App):
     def build(self):
